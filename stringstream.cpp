f3d835fbb81f05026a4c71b1d27d357d439f2105
Parmera <40071746+JoaoAugustoI@users.noreply.github.com>
2020-03-04 14:02:48 -0300

    Create stringstream.cpp

---
+++ stringstream.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int vet[101];
+	
+	string s;
+	
+	int N;
+	cin>>N;
+	cin.ignore();
+	while(N--)
+	{
+		getline(cin,s);
+		strinstream st(s);
+		int i = 0;
+		while(st>>vet[i++]);
+	}
+}
